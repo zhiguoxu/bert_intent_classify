420985ba4152b7dbd4721b7351529d6125280712
zhiguo <zhiguoxu2004@163.com>
2026-07-01 09:08:08 +0000

    opt download

--- train/script/download_model.sh
+++ train/script/download_model.sh
@@ -1,1 +1,5 @@
+#!/usr/bin/env bash
+# 服务器无法直连 huggingface.co（Network is unreachable），使用国内镜像 hf-mirror.com
+export HF_ENDPOINT=https://hf-mirror.com
+
 hf download hfl/chinese-roberta-wwm-ext-large --local-dir ./chinese-roberta-wwm-ext-large --exclude "*.h5" --exclude "*.msgpack"
